563afdabd393bcc5c5051cf9d931ec0037876f6b
Martin5009 <msusanto@hmc.edu>
2024-10-23 20:50:12 -0500

    Merged with files from work

--- components/led_matrix/led_matrix.c
+++ components/led_matrix/led_matrix.c
@@ -30,10 +30,13 @@ esp_err_t led_matrix_init(led_matrix_config_t* led_config, led_matrix_handle_t l
 
     gptimer_set_alarm_action(led_config->refresh_timer, &alarm_config);
 
-    //Create led matrix struct
+   //Create led matrix struct
     out_handle->refresh_rate = led_config->refresh_rate;
     out_handle->width = led_config->width;
     out_handle->height = led_config->height;
+    out_handle->pwm_cnt = led_config->pwm_cnt;
+    out_handle->pwm_level = led_config->pwm_level;
+    out_handle->refresh_task = led_config->refresh_task;
     out_handle->refresh_timer = led_config->refresh_timer;
     out_handle->io_assign = led_config->io_assign;
 
@@ -51,22 +54,85 @@ err:
     return ret;
 }
 
-esp_err_t led_matrix_rm(led_matrix_handle_t led_matrix_handle)
-{
-    return ESP_OK;
-}
-
 esp_err_t led_matrix_start_refresh(led_matrix_handle_t led_matrix_handle)
-{
+{   
+    xTaskCreate(led_matrix_refresh_task, "refresh_matrix", 5000, led_matrix_handle, 5, &led_matrix_handle->refresh_task);
+    gptimer_enable(led_matrix_handle->refresh_timer);
     return ESP_OK;
 }
 
 esp_err_t led_matrix_stop_refresh(led_matrix_handle_t led_matrix_handle)
 {
+    vTaskDelete(led_matrix_handle->refresh_task);
+    gptimer_disable(led_matrix_handle->refresh_timer);
     return ESP_OK;
 }
 
-static bool IRAM_ATTR led_matrix_refresh_cb(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data)
+static void led_matrix_refresh_task(void *pvParameters)
+{   
+    const TickType_t xBlockTime = LED_MATRIX_REFRESH_TIMEOUT_THRESHOLD;
+    uint32_t ulNotifiedValue;
+
+    led_matrix_handle_t led_matrix_handle = (led_matrix_handle_t)pvParameters;
+
+    //initialize pwm counter
+    led_matrix_handle->pwm_cnt = 0;
+
+    for (;;)
+    {
+        ulNotifiedValue = ulTaskNotifyTake(pdTRUE, xBlockTime);
+
+        if (ulNotifiedValue > 0)
+        {
+            //perform refresh
+            led_matrix_write_screen(led_matrix_handle);
+
+            //update pwm counter
+            if(led_matrix_handle->pwm_cnt >= led_matrix_handle->pwm_level)
+            {
+                led_matrix_handle->pwm_cnt = 0;
+            }
+            else
+            {
+                led_matrix_handle->pwm_cnt++;
+            }
+
+        }
+        else
+        {
+            //timeout error
+            printf("Timeout: 5000ms since last refresh interrupt");
+        }
+    }
+}
+
+void led_matrix_set_buffer(led_matrix_handle_t led_matrix_handle, led_matrix_rgb_t *buffer)
 {
-    return true;
+    //Should set the contents of the led matrix frame buffer to a fixed image.
+    //This function will eventually be replaced by a number of tasks which
+    //will dynamically update the buffer depending on the desired operation (e.g. clock).
+}
+
+static void led_matrix_write_screen(led_matrix_handle_t led_matrix_handle)
+{
+    //Should write to all rows of the matrix based on the contents
+    //of the frame buffer and the current value of pwm_cnt.
+}
+
+static bool IRAM_ATTR led_matrix_refresh_cb(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data)
+{   
+    gptimer_stop(timer);
+    
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    TaskHandle_t task_handle = (TaskHandle_t)user_data;
+
+    vTaskNotifyGiveFromISR(task_handle, &xHigherPriorityTaskWoken);
+
+    gptimer_start(timer);
+
+    //force context switch upon exit
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken); 
+
+    return (xHigherPriorityTaskWoken == true);
 }
--- components/led_matrix/led_matrix.h
+++ components/led_matrix/led_matrix.h
@@ -2,6 +2,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
 #include "esp_check.h"
 #include "esp_log.h"
 #include "esp_types.h"
@@ -10,10 +13,12 @@
 #include "driver/gptimer.h"
 #include "driver/gpio.h"
 
+#define LED_MATRIX_REFRESH_TIMEOUT_THRESHOLD pdMS_TO_TICKS(5000)
+
 enum led_matrix_pwm_levels{
-    LED_MATRIX_PWM_1 = 1,
-    LED_MATRIX_PWM_3 = 3,
-    LED_MATRIX_PWM_256 = 256};
+    LED_MATRIX_PWM_1 = 0,
+    LED_MATRIX_PWM_3 = 2,
+    LED_MATRIX_PWM_256 = 255};
 typedef enum led_matrix_pwm_levels led_matrix_pwm_levels;
 
 typedef struct {
@@ -41,6 +46,9 @@ typedef struct {
     uint8_t refresh_rate;           //display refresh rate capped at 200Hz
     uint8_t width;
     uint8_t height;
+    uint8_t pwm_cnt;
+    led_matrix_pwm_levels pwm_level;
+    TaskHandle_t refresh_task;
     gptimer_handle_t refresh_timer; //used to generate periodic interrupts for display refresh
     led_matrix_io_t *io_assign;
 } led_matrix_config_t;
@@ -49,6 +57,9 @@ typedef struct {
     uint8_t refresh_rate;
     uint8_t width;
     uint8_t height;
+    uint8_t pwm_cnt;
+    led_matrix_pwm_levels pwm_level;
+    TaskHandle_t refresh_task;
     led_matrix_rgb_t *buffer_1;
     gptimer_handle_t refresh_timer;
     led_matrix_io_t *io_assign;
@@ -58,13 +69,16 @@ typedef led_matrix_t *led_matrix_handle_t;
 
 esp_err_t led_matrix_init(led_matrix_config_t* led_config, led_matrix_handle_t led_matrix);
 
-esp_err_t led_matrix_rm(led_matrix_handle_t led_matrix_handle);
-
 esp_err_t led_matrix_start_refresh(led_matrix_handle_t led_matrix_handle);
 
 esp_err_t led_matrix_stop_refresh(led_matrix_handle_t led_matrix_handle);
 
-static bool IRAM_ATTR led_matrix_refresh_cb(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data);
+static void led_matrix_refresh_task(void *pvParameters);
 
+void led_matrix_set_buffer(led_matrix_handle_t led_matrix_handle, led_matrix_rgb_t *buffer);
+
+static void led_matrix_write_screen(led_matrix_handle_t led_matrix_handle);
+
+static bool IRAM_ATTR led_matrix_refresh_cb(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data);
 
 
